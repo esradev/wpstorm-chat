088c1c81611fdba0fc5771bc07d23b7254818584
Wpstorm <wpstormdev@gmail.com>
2023-09-10 23:45:07 +0330

    Fix: Fixed some style bugs on sidebar of admin dashboard.

--- src/admin-page/layouts/SidebarRoutes.js
+++ src/admin-page/layouts/SidebarRoutes.js
@@ -1,20 +1,21 @@
 import Settings from "../pages/Settings"
+import Style from "../pages/Style"
 
 import {TbSettings2, TbLogin} from "react-icons/tb"
 
 const SidebarRoutes = [
     {
         path: '/',
-        label: 'General Settings',
+        label: 'Dashboard',
         component: Settings,
         icon: TbSettings2,
     },
-    // {
-    //     path: '/login',
-    //     label: 'Login Settings',
-    //     component: Login,
-    //     icon: TbLogin,
-    // },
+    {
+        path: '/style',
+        label: "Style's",
+        component: Style,
+        icon: TbLogin,
+    },
 ]
 
 export default SidebarRoutes;
